3d3cd1d33b4e0e7fe346c281c3cc8e33ea9a7be0
CHRISTIAN G. GALLEGOS YAGUACHI <cgallegos@inventar-tech.ad>
2024-11-25 12:09:01 -0500

    Rediseño de la pantalla principal

--- grabar-voz/obj/Debug/net8.0-windows/grabar-voz.AssemblyInfo.cs
+++ grabar-voz/obj/Debug/net8.0-windows/grabar-voz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("grabar-voz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3677969271b95cb7dfe88be1a6bf0c95ed886963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8303679a6672ffa6d4d19514118823811238170")]
 [assembly: System.Reflection.AssemblyProductAttribute("grabar-voz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("grabar-voz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
