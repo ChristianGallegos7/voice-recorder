d8303679a6672ffa6d4d19514118823811238170
CHRISTIAN G. GALLEGOS YAGUACHI <cgallegos@inventar-tech.ad>
2024-11-25 11:54:46 -0500

    Rediseño, faltan detalles

--- grabar-voz/obj/Debug/net8.0-windows/grabar-voz.AssemblyInfo.cs
+++ grabar-voz/obj/Debug/net8.0-windows/grabar-voz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("grabar-voz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a26aa8c94afc930a2f817ee18f8efed2951bc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3677969271b95cb7dfe88be1a6bf0c95ed886963")]
 [assembly: System.Reflection.AssemblyProductAttribute("grabar-voz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("grabar-voz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
